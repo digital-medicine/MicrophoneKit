59d482cf53aea9aee3db547cc100ae54264aa57b
Imad Azizi <s47iaziz@uni-bonn.de>
2025-04-19 23:15:53 +0200

    added the writeWaveFile to write audio chunks for faster processing

--- Sources/MicrophoneKit/Audio/WhisperAudio/WhisperUtils.swift
+++ Sources/MicrophoneKit/Audio/WhisperAudio/WhisperUtils.swift
@@ -22,3 +22,52 @@ public func decodeWaveFile(_ url: URL) throws -> [Float] {
     
     return floats
 }
+
+
+/// Helper to write float samples as WAV file with standard header
+public func writeWaveFile(samples: [Float], to url: URL) throws {
+    var data = Data()
+    let sampleCount = samples.count
+    let sampleRate: UInt32 = 16000
+    let bitsPerSample: UInt16 = 16
+    let numChannels: UInt16 = 1
+    let byteRate = sampleRate * UInt32(bitsPerSample / 8)
+
+    // Header chunk
+    data.append("RIFF".data(using: .ascii)!)
+    data.append(UInt32(36 + sampleCount * 2).littleEndianData)
+    data.append("WAVE".data(using: .ascii)!)
+    data.append("fmt ".data(using: .ascii)!)
+    data.append(UInt32(16).littleEndianData)
+    data.append(UInt16(1).littleEndianData)
+    data.append(numChannels.littleEndianData)
+    data.append(sampleRate.littleEndianData)
+    data.append(byteRate.littleEndianData)
+    data.append(UInt16(numChannels * bitsPerSample / 8).littleEndianData)
+    data.append(bitsPerSample.littleEndianData)
+    data.append("data".data(using: .ascii)!)
+    data.append(UInt32(sampleCount * 2).littleEndianData)
+
+    // PCM Data
+    for float in samples {
+        let clamped = max(-1.0, min(1.0, float))
+        let int16 = Int16(clamped * Float(Int16.max))
+        data.append(int16.toBytes())
+    }
+
+    try data.write(to: url)
+}
+
+fileprivate extension FixedWidthInteger {
+    var littleEndianData: Data {
+        var value = self.littleEndian
+        return withUnsafeBytes(of: &value) { Data($0) }
+    }
+}
+
+extension Int16 {
+    func toBytes() -> Data {
+        var value = self.littleEndian
+        return withUnsafeBytes(of: &value) { Data($0) }
+    }
+}
